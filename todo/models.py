654968146a02bcb524594226af56cee16020b1db
linux <fa.mahabadi@yahoo.com>
2025-05-22 16:49:21 +0330

    Todo|Update|model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse_lazy
 
-# Create your models here.
+
+class TodoModel(models.Model):
+    title = models.CharField(max_length=250)
+    detail = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse_lazy("create_todo")
