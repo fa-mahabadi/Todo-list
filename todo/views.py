ae4566d1f5ad337ba7aa41ee62d22722abd18619
linux <fa.mahabadi@yahoo.com>
2025-05-25 12:56:04 +0330

    Todo|Update|view

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from django.views.generic.edit import DeleteView
 from .models import TodoModel
 from .forms import TodoForm
+from django.urls import reverse_lazy
+
 
 class CreateTodo(CreateView):
-    model=TodoModel
-    template_name="todo/create.html"
-    form_class=TodoForm
+    model = TodoModel
+    template_name = "todo/create.html"
+    form_class = TodoForm
+    success_url=reverse_lazy("list_todo")
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+
+class ListTodo(ListView):
+    model = TodoModel
+    template_name = "todo/list.html"
+    queryset=TodoModel.objects.order_by("-created_at")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = TodoForm()
+        context['last_articles']=TodoModel.objects.order_by('-created_at')[:2]
+        return context
+
+
+class DeleteTodo(DeleteView):
+    model = TodoModel
+    success_url = reverse_lazy("list_todo")
