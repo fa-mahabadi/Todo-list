0c26591a242423a645e688577c2b1681d6da9189
linux <fa.mahabadi@yahoo.com>
2025-05-25 12:55:46 +0330

    Todo|Update|model

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.urls import reverse_lazy
-
+from django.contrib.auth.models import User
 
 class TodoModel(models.Model):
     title = models.CharField(max_length=250)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
     detail = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
