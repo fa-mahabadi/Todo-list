2b7cc26bd2fdb8cf64d6654d7466d84babf8d36b
linux <fa.mahabadi@yahoo.com>
2025-05-25 12:56:31 +0330

    Todo|Update|url

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateTodo
+from .views import CreateTodo,ListTodo,DeleteTodo
 
 urlpatterns=[
     path("create/",CreateTodo.as_view(),name="create_todo"),
+    path("list/",ListTodo.as_view(),name="list_todo"),
+    path("delete/<int:pk>/",DeleteTodo.as_view(),name="delete_todo")
 ]
