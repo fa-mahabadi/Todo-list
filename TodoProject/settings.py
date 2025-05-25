5b2d0b1c067b0f96155d2590096b828cd2f90e9c
linux <fa.mahabadi@yahoo.com>
2025-05-25 12:54:04 +0330

    Todoproject|Update|settings

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'todo',
+    'accounts',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -123,8 +124,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media File
+MEDIA_URL="/media/"
+MEDIA_ROOT=BASE_DIR/'uploads'
+
+# login redirect URL
+LOGIN_REDIRECT_URL="list_todo"
