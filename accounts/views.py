a0448a0ad06c71c19e361bfff55d76b20e851c47
linux <fa.mahabadi@yahoo.com>
2025-05-26 17:19:38 +0330

    Account|Update|view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class SignUp(CreateView):
+    form_class=RegisterForm
+    template_name="registration/SignUp.html"
+    success_url=reverse_lazy("login")
