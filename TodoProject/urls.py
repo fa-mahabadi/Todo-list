2fc9649d2123bfde8039cb0d031e1247bf377f48
linux <fa.mahabadi@yahoo.com>
2025-05-26 17:16:52 +0330

    TodoProject|Update|urls

--- TodoProject/urls.py
+++ TodoProject/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("todo/",include("todo.urls")),
+    path("accounts/",include("accounts.urls")),
     path("accounts/",include("django.contrib.auth.urls")),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
