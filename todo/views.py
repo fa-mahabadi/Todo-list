a1657ad2bf15a4434f2ad3d1b483c503c2a12b9b
linux <fa.mahabadi@yahoo.com>
2025-05-27 13:36:47 +0330

    Todo|Update|view

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
-from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import DeleteView,UpdateView
 from .models import TodoModel
 from .forms import TodoForm
 from django.urls import reverse_lazy
 
 
-class CreateTodo(CreateView):
+class CreateTodo(LoginRequiredMixin,CreateView):
     model = TodoModel
     template_name = "todo/create.html"
     form_class = TodoForm
@@ -17,24 +19,32 @@ class CreateTodo(CreateView):
         return super().form_valid(form)
 
 
-class ListTodo(ListView):
+class ListTodo(LoginRequiredMixin,ListView):
     model = TodoModel
     template_name = "todo/list.html"
-    queryset=TodoModel.objects.order_by("-created_at")
+
+    def get_queryset(self):
+        return TodoModel.objects.filter(author=self.request.user).order_by("-created_at")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["form"] = TodoForm()
-        context['last_articles']=TodoModel.objects.order_by('-created_at')[:2]
+        context['last_articles']=TodoModel.objects.filter(author=self.request.user).order_by('-created_at')[:2]
         return context
+    
+class UpdateTodo(LoginRequiredMixin,UpdateView):
+    model=TodoModel
+    fields=("title","detail")
+    success_url=reverse_lazy("list_todo")
+    template_name="todo/update.html"
 
-
-class DeleteTodo(DeleteView):
+class DeleteTodo(LoginRequiredMixin,DeleteView):
     model = TodoModel
     success_url = reverse_lazy("list_todo")
 
 
+@login_required
 def todo_search(request):
     query=request.GET.get("q")
-    qs=TodoModel.objects.filter(title__contains=query)
+    qs=TodoModel.objects.filter(title__contains=query , author=request.user)
     return render(request,"todo/result.html",context={"todos":qs})
